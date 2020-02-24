adbc07231db7c7792a44b8102353d63344c9c521
Paul <mihali.paul123@gmail.com>
2020-02-24 17:50:13 +0000

    Minor fixes

--- qa/context_processor.py
+++ qa/context_processor.py
@@ -1,6 +1,7 @@
 from .models import Topic
 
 def top_topics(request):
+    """ Returns the names of the top 8 topics """
     return {
-        'top_topics': Topic.objects.all().order_by('-times_used')[:8]
+        'top_topics': Topic.objects.all().order_by('-times_used')[:8].values_list('name', flat=True)
     }
